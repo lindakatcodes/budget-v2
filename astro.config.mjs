90acbbad43f461e6481e290d346484925a1d25ed
LindaKat <lindakt16@gmail.com>
2024-06-12 15:46:46 -0500

    added clerk, setup components and made sure auth worked properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
+import clerk from "astro-clerk-auth";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
+  integrations: [
+    db(),
+    clerk({
+      afterSignInUrl: "/",
+      afterSignUpUrl: "/",
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
